abeb2957c87237752ea5677e3a93be46723a3277
Tom Whitcomb <Tom_Whitcomb@intuit.com>
2015-03-02 22:45:36 -0800

    added recur and refactoring

--- src/clojure_gol/core.clj
+++ src/clojure_gol/core.clj
@@ -2,21 +2,15 @@
   (:gen-class)
   (:require [clojure.test :refer :all])
 )
-
+(defn surrounding-peers [x y] [[(+ x 1) y] [(- x 1) y] [x (+ y 1)][x (- y 1)][(- x 1) (- y 1)][(+ x 1) (+ y 1)][(- x 1) (+ y 1)][(+ x 1) (- y 1)]])
 (defn live-neighbours [[x y] space]
-  (let [x [
-          (space [(+ x 1) y] :d) 
-          (space [(- x 1) y] :d) 
-          (space [x (+ y 1)] :d) 
-          (space [x (- y 1)] :d) 
-          (space [(- x 1) (- y 1)] :d) 
-          (space [(+ x 1) (+ y 1)] :d) 
-          (space [(- x 1) (+ y 1)] :d) 
-          (space [(+ x 1) (- y 1)] :d) 
-         ]]
-    (count (filter #(= % :l) x))
+  (let [
+        values (map #(space % :d) (surrounding-peers x y))
+        ]
+    (count (filter #(= % :l) values))
   )
 )
+
 (defn evolve [ [k v] space]
   (let [
         live (live-neighbours k space)
@@ -36,7 +30,7 @@
   )  
 )
 (defn pad-cell [[x y]]
-  [[(+ x 1) y] :d,[(- x 1) y] :d,[x (+ y 1)] :d,[x (- y 1)] :d,[(- x 1) (- y 1)] :d,[(+ x 1) (+ y 1)] :d,[(- x 1) (+ y 1)] :d,[(+ x 1) (- y 1)] :d ]
+  (apply concat (for [a (surrounding-peers x y) ] [a :d]))
 )
 
 (defn select-alive [space] (filter (fn [[k v]] (= v :l)) space))
@@ -52,12 +46,14 @@
   )
 )  
 (defn run1 [tick space ] 
-  (println (select-alive space))
-  (let [padded-space (pad-the-board space)]
-    (cond 
-      (<= tick 0) padded-space
-      :else (run1 (- tick 1) (into {} (map (fn [[k v]](evolve [k v] padded-space)) padded-space)))
-    )  
+  (loop [tick1 tick space1 space]
+    (println (select-alive space1))
+    (let [padded-space (pad-the-board space1)]
+      (cond 
+        (<= tick1 0) padded-space
+        :else (recur (- tick1 1) (into {} (map (fn [[k v]](evolve [k v] padded-space)) padded-space)))
+      )  
+    )
   )  
 )
 (defn run [n space] 
@@ -68,4 +64,5 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!")
+  (println "yo mamma")
 )
